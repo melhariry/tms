5acfd496d897c4c0a7ac183d6df7fc904f5470c4
MohamedAsaker <mohammed.assaker@gmail.com>
2016-02-25 20:42:17 +0200

    update

---
+++ Update_AP/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Release configuration
+CND_PLATFORM_Release=Gnueabi-Windows
+CND_ARTIFACT_DIR_Release=dist/V3/App
+CND_ARTIFACT_NAME_Release=Update_AP
+CND_ARTIFACT_PATH_Release=dist/V3/App/Update_AP
+CND_PACKAGE_DIR_Release=dist/Release/Gnueabi-Windows/package
+CND_PACKAGE_NAME_Release=updateap.tar
+CND_PACKAGE_PATH_Release=dist/Release/Gnueabi-Windows/package/updateap.tar
+# Debug configuration
+CND_PLATFORM_Debug=Gnueabi-Windows
+CND_ARTIFACT_DIR_Debug=dist/V3/Debug
+CND_ARTIFACT_NAME_Debug=Update_AP
+CND_ARTIFACT_PATH_Debug=dist/V3/Debug/Update_AP
+CND_PACKAGE_DIR_Debug=dist/Debug/Gnueabi-Windows/package
+CND_PACKAGE_NAME_Debug=updateap.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Gnueabi-Windows/package/updateap.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
